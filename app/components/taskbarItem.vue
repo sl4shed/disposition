28b3329d7356a840a95d586792671235f7097374
Andrew <sl4shed@proton.me>
2026-09-17 07:32:43 +0300

    taskbar work

--- app/components/taskbarItem.vue
+++ app/components/taskbarItem.vue
@@ -1,7 +1,50 @@
 <script lang="ts" setup>
+import type { Component } from 'vue';
 
+defineProps<{
+    app: Component
+}>()
+
+function open() {
+    console.log("open");
+}
 </script>
 
 <template>
-    
+    <div class="item" @click="open">
+        <button class="icon"></button>
+        <div class="open"></div>
+    </div>
+
 </template>
+
+<style scoped>
+.item {
+    width: 25px;
+    height: 100%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.icon {
+    border: none;
+    width: 25px;
+    height: 25px;
+
+    background-image: url("~/assets/icons/shop.png");
+    background-size: cover;
+}
+
+.open {
+    position: absolute;
+    bottom: 0;
+    width: 20px;
+    left: 2.5px;
+
+    height: 3px;
+    border-top-left-radius: 3px;
+    border-top-right-radius: 3px;
+}
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,11 +2,14 @@
 import "./index.css"
 import Welcome from '~/apps/welcome.vue';
 import Taskbar from '~/components/taskbar.vue';
+import TaskbarItem from '~/components/taskbarItem.vue';
 
 </script>
 
 <template>
     <Welcome />
 
-    <Taskbar />
+    <Taskbar>
+        <TaskbarItem :app="Welcome"/>
+    </Taskbar>
 </template>
